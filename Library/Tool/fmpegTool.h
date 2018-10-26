ae5a669a3a1499d20a5eb2fab5a4cd583689be45
“stanhu” <3421902@qq.com>
2018-10-26 15:50:28 +0800

    add ffmpeg

---
+++ Library/Tool/fmpegTool.h
@@ -0,0 +1,19 @@
+//
+//  fmpegTool.h
+//  iOSDemo
+//
+//  Created by Stan Hu on 2018/10/25.
+//  Copyright © 2018 Stan Hu. All rights reserved.
+//
+#import <libavcodec/avcodec.h>
+#import <libavformat/avformat.h>
+#import <libswscale/swscale.h>
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface fmpegTool : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Library/Tool/fmpegTool.m
@@ -0,0 +1,13 @@
+//
+//  fmpegTool.m
+//  iOSDemo
+//
+//  Created by Stan Hu on 2018/10/25.
+//  Copyright © 2018 Stan Hu. All rights reserved.
+//
+
+#import "fmpegTool.h"
+
+@implementation fmpegTool
+
+@end
