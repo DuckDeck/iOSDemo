c46e0bd6c21ba94e98ab09378c0e6ab86d1dcc8f
“stanhu” <3421902@qq.com>
2018-11-12 18:33:00 +0800

    process code

---
+++ Console/MessagForward/HydronForward.h
@@ -0,0 +1,19 @@
+//
+//  HydronForward.h
+//  Console
+//
+//  Created by Stan Hu on 2018/11/12.
+//  Copyright © 2018 Stan Hu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HydronForward : NSObject
+-(NSString*)hydronId;
+@end
+
+NS_ASSUME_NONNULL_END
+
+
