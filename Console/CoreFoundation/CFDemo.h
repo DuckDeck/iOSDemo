bb4e812f61d605a3c8420dd113eb29816801e4ad
“stanhu” <3421902@qq.com>
2018-11-13 18:29:24 +0800

    process coide

---
+++ Console/CoreFoundation/CFDemo.h
@@ -0,0 +1,17 @@
+//
+//  CFDemo.h
+//  Console
+//
+//  Created by Stan Hu on 2018/11/13.
+//  Copyright © 2018 Stan Hu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CFDemo : NSObject
++(void)test;
+@end
+
+NS_ASSUME_NONNULL_END
