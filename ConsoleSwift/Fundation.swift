11cdbfb6cbac345557dc08eb2e7d12f7b72338dc
“stanhu” <3421902@qq.com>
2019-07-09 15:53:46 +0800

    proess code

---
+++ ConsoleSwift/Fundation.swift
@@ -0,0 +1,11 @@
+//
+//  Fundation.swift
+//  ConsoleSwift
+//
+//  Created by Stan Hu on 2019/7/1.
+//  Copyright © 2019 Stan Hu. All rights reserved.
+//
+
+import UIKit
+
+
--- Library/Tool/ffmpegTool.h
+++ Library/Tool/ffmpegTool.h
@@ -14,6 +14,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface ffmpegTool : NSObject
 +(void)initProject;
+-(AVFormatContext *)createFormatContextByFilePath:(NSString *)path;
 @end
 
 NS_ASSUME_NONNULL_END
