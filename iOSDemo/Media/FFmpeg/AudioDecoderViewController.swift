98f253a93597a5ce5663d6dd86c4b1420d92ee7b
“stanhu” <3421902@qq.com>
2019-08-02 16:38:30 +0800

    add record audio

--- iOSDemo/Media/FFmpeg/AudioDecoderViewController.swift
+++ iOSDemo/Media/FFmpeg/AudioDecoderViewController.swift
@@ -15,11 +15,12 @@ class AudioDecoderViewController: UIViewController {
     let btnStop = UIButton()
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.title = "Audio Queue 处理音频"
         view.backgroundColor = UIColor.white
         AudioQueueCaptureManager.getInstance().startAudioCapture()
       
         btnRecord.title(title: "开始录音").color(color: UIColor.red).addTo(view: view).snp.makeConstraints { (m) in
-            m.left.equalTo(100)
+            m.left.equalTo(50)
             m.top.equalTo(100)
             m.width.equalTo(100)
             m.height.equalTo(50)
@@ -27,9 +28,9 @@ class AudioDecoderViewController: UIViewController {
         
         btnRecord.addTarget(self, action: #selector(record), for: .touchUpInside)
         
-        btnStop.title(title: "开始录音").color(color: UIColor.red).addTo(view: view).snp.makeConstraints { (m) in
+        btnStop.title(title: "停止录音").color(color: UIColor.red).addTo(view: view).snp.makeConstraints { (m) in
             m.top.equalTo(100)
-            m.right.equalTo(-100)
+            m.right.equalTo(-50)
             m.width.equalTo(100)
             m.height.equalTo(50)
         }
