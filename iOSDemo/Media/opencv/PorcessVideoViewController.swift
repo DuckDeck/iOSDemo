933265de80b7b6a45da5c314e52de627558fde04
“stanhu” <3421902@qq.com>
2019-09-26 14:23:03 +0800

    add video

---
+++ iOSDemo/Media/opencv/PorcessVideoViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PorcessVideoViewController.swift
+//  iOSDemo
+//
+//  Created by Stan Hu on 2019/9/26.
+//  Copyright © 2019 Stan Hu. All rights reserved.
+//
+
+import UIKit
+
+class PorcessVideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
