354d9e379604305a8c3daf54c45c68ed9b10c590
“stanhu” <3421902@qq.com>
2019-10-22 15:30:35 +0800

    process code

---
+++ Console/Runtime/MessageSend.m
@@ -0,0 +1,19 @@
+//
+//  MessageSend.m
+//  Console
+//
+//  Created by Stan Hu on 2019/10/22.
+//  Copyright © 2019 Stan Hu. All rights reserved.
+//
+
+#import "MessageSend.h"
+
+@implementation MessageSend
+
+@end
+
+@implementation Person
+
+
+
+@end
