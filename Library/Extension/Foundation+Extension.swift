67a35d447ec8593d8b7bc032b0a279456a46694c
“stanhu” <3421902@qq.com>
2020-03-16 14:19:41 +0800

    process code

--- Library/Extension/Foundation+Extension.swift
+++ Library/Extension/Foundation+Extension.swift
@@ -196,10 +196,8 @@ extension String{
     public func hmac(key:String)->String{
         let utf8 = cString(using: .utf8)
         let keyData = key.cString(using: .utf8)
-        
         var digest = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
         CCHmac(CCHmacAlgorithm(kCCHmacAlgMD5), keyData, strlen(keyData!), utf8, strlen(utf8!), &digest)
-//        CC_MD5(utf8, CC_LONG(utf8!.count - 1), &digest)
         return digest.reduce(""){$0 + String(format:"%02X",$1)}
     }
     
