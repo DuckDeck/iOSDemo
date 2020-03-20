c314d980b3ea44e68c99e4d10f26d474e3f2066d
“stanhu” <3421902@qq.com>
2020-03-20 09:30:30 +0800

    process code

--- ConsoleSwift/main.swift
+++ ConsoleSwift/main.swift
@@ -219,9 +219,6 @@ txt.enumerateSubstrings(in: t!, options: String.EnumerationOptions.bySentences) 
     print("sentence\(str)")
 }
 
-
-
-
 RegexTest.testRegex1()
 RegexTest.testRegex2()
 RegexTest.testRegex3()
@@ -230,3 +227,23 @@ RegexTest.testRegex5()
 
 //ThreadTest.ThreadName1()
 ThreadTest.ThreadName2()
+
+
+class Delegate<Input,Output>{
+    private var block:((Input)->Output?)?
+    func delegate<T:AnyObject>(on target: T,block:((T,Input)->Output)?){
+        self.block = {[weak target] input in
+            guard let target = target  else {
+                return nil
+            }
+            return block?(target,input)
+        }
+    }
+    
+    func call(_ input : Input)->Output{
+        return (block?(input))!
+    }
+}
+
+
+
