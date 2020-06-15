ba465c9cc58b184b4ceb190a84677c430d5d0014
DuckDeck <3421902@qq.com>
2020-06-15 09:06:19 +0800

    process code

--- iOSDemoOC/ViewController.m
+++ iOSDemoOC/ViewController.m
@@ -20,13 +20,33 @@
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor blueColor];
     
-    [self test1];
+   // [self test1];
     
     
     
     
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [super touchesBegan:touches withEvent:event];
+    NSLog(@"1--------");
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+         NSLog(@"2--------");
+//        [self performSelector:@selector(test) withObject:nil];  //1243
+//        [self performSelector:@selector(test) withObject:nil afterDelay:0]; //123 ,selector 不执行了
+ //       [self performSelector:@selector(test) onThread:[NSThread currentThread] withObject:nil waitUntilDone:NO]; //123
+        [self performSelector:@selector(test) onThread:[NSThread currentThread] withObject:nil waitUntilDone:YES]; //1234
+
+         NSLog(@"3--------");
+    });
+    
+}
+
+
+-(void)test{
+    NSLog(@"4--------");
+
+}
 
 -(void)test1{
     [self.thread setName:@"StopRunloopThread"];
