9b389db4a691921e2c79348a4dcffa62fbbd4172
shadowedge <3421902@qq.com>
2023-08-31 09:31:25 +0800

    加入logtool

---
+++ iOSDemo/Base/Log/LogTool.swift
@@ -0,0 +1,37 @@
+//
+//  LogTool.swift
+//  iOSDemo
+//
+//  Created by Stan Hu on 2021/11/17.
+//  Copyright © 2021 Stan Hu. All rights reserved.
+//
+
+import Foundation
+import WCDBSwift
+class LogTool{
+    static let db = Database(withPath: "\(NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.libraryDirectory, .userDomainMask, true).first!)/log.db")
+    
+    init() {
+
+    }
+    
+    
+}
+
+enum logLevel:Int{
+    case debug = 0
+    case info
+    case warn
+    case error
+    case fatal
+}
+
+
+struct log{
+    var time:Date = Date()
+    var level = logLevel.info
+    var content = ""
+    
+}
+
+
